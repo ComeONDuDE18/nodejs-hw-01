e9e5cdf148b3297a07e6ae25657bc4d89983e0bd
ComeONDuDE18 <vibilous6@gmail.com>
2025-09-08 14:31:27 +0200

    last one

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,21 @@
-export const removeLastContact = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const removeLastContact = async () => {
+    try {
+        const data = await readContacts();
+        if (data.length === 0) {
+            console.log('No contacts to remove');
+            return;
+        }
+        const removeLast = data.slice(0, data.length - 1);
+        await writeContacts(removeLast);
+        console.log('Removed last');
+    }
+          catch (error) {
+        console.error("error", error);
+    }
+
+};
 
 removeLastContact();
