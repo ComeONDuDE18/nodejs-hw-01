473a7a8ffde5744f27a3f656a08e86051fa9b4a8
ComeONDuDE18 <vibilous6@gmail.com>
2025-09-08 14:21:48 +0200

    step 6

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,15 @@
-export const countContacts = async () => {};
+import fs from 'node:fs/promises';
+
+export const countContacts = async () => {
+    try {
+        const files = await fs.readFile('src/db/db.json', 'utf-8');
+        const contacts = JSON.parse(files);
+        return contacts.length;
+
+    } catch (error) {
+        console.error('Error reading contacts:', error);
+        return error;
+    }
+};
 
 console.log(await countContacts());
