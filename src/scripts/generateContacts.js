036d304cf83b969afcaaad591119ec20be61fa6d
ComeONDuDE18 <vibilous6@gmail.com>
2025-09-08 13:45:26 +0200

    step 3

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,22 @@
-const generateContacts = async (number) => {};
 
+import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+    try {
+    const contacts = await readContacts();
+    const newContact = Array.from({ length: number }, () => ( createFakeContact()));
+    const updatedContacts = contacts.concat(...newContact);
+    await writeContacts(updatedContacts);
+    console.log(`${number} contacts generated and saved.`);
+    }
+
+    catch (error) {
+        console.error('Error generating contacts:', error);
+
+    }
+
+};
 generateContacts(5);
+
